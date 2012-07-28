cb1a9fead44a4e7a0412630cba24df2581e669a6
Travis Parks <jehugaleahsa@gmail.com>
2012-07-27 21:18:01 -0400

    Redesigned and implemented multipatches. Now matching mock.patch interface.

--- multipatch.py
+++ multipatch.py
@@ -1,68 +1,62 @@
 import mock
 
 
-def patch(target=''):
-    collection = PatchCollection()
-    collection = collection.patch(target)
+def multipatch(*multipatches, **patches):
+    collection = _PatchCollection()
+
+    # aggregate other multipatchers
+    for other in multipatches:
+        collection._copyPatches(other)
+
+    # add new patches
+    for name in patches:
+        patch = patches[name]
+        collection._addPatch(name, patch)
+
     return collection
 
-class PatchCollection(object):
 
+class _PatchCollection(object):
     def __init__(self):
-        self.__isPatched = False
+        self.__isStarted = False
         self.__patches = {}
-        self.__patchers = {}
+        self.__mocks = {}
 
-    def patch(self, target):
-        # ignore blank targets
-        if not target.strip():
-            return self
+    def _addPatch(self, name, patch):
+        if name in self.__patches:
+            format = 'A patch with the same name already exists: {0}.'
+            message = format.format(name)
+            raise ValueError(message)
+        self.__patches[name] = patch
 
-        # blow up on duplicate names
-
-        name = self.__getName(target)
-
-        newCollection = PatchCollection()
-        newCollection.__patches = self.__patches.copy()
-        newCollection.__patches[name] = mock.patch(target)
-        return newCollection
-
-    def __getName(self, target):
-        name = target
-        index = name.rfind('.')
-        if index != -1:
-            name = name[(index + 1):]
-        return name
+    def _copyPatches(self, other):
+        for name in other.__patches:
+            patch = other.__patches[name]
+            self._addPatch(name, patch)
 
     def start(self):
-        if not self.__isPatched:
-            self.__isPatched = True
+        if not self.__isStarted:
+            self.__isStarted = True
             for name in self.__patches:
                 patch = self.__patches[name]
-                patcher = patch.start()
-                self.__patchers[name] = patcher
-
-    @property
-    def isStarted(self):
-        return self.__isPatched
+                self.__mocks[name] = patch.start()
+        return self
 
     def stop(self):
-        if self.__isPatched:
+        if self.__isStarted:
             for name in self.__patches:
                 patch = self.__patches[name]
                 patch.stop()
-            self.__patchers.clear()
-            self.__isPatched = False
+            self.__mocks.clear() # not born from a unit test
+            self.__isStarted = False
 
     def __enter__(self):
-        self.start()
-        return self
-    
+        return self.start()
+
     def __exit__(self, ExceptionType, exception, traceback):
         self.stop()
 
     def __getattr__(self, name):
-        if not self.__isPatched or name not in self.__patchers:
-            return super(PatchCollection, self).__getattr__()
-        patcher = self.__patchers[name]
-        return patcher
+        if not self.__isStarted or name not in self.__mocks:
+            raise AttributeError()
+        return self.__mocks[name]
--- tests/unit/multipatch_tests.py
+++ tests/unit/multipatch_tests.py
@@ -1,82 +1,128 @@
 import mock
 import unittest
 
-from multipatch import patch, PatchCollection
+from multipatch import multipatch
 
 
-class PatchTester(unittest.TestCase):
-    """Tests the `patch` method."""
+class MultipatchTester(unittest.TestCase):
+    """Tests the multipatch function."""
 
-    def testPatch_noModuleName_createsEmptyPatchCollection(self):
-        """
-        Calling patch with no target returns an empty collection.
-        """
-        collection = patch()
-        self.assertIsInstance(
-            collection, PatchCollection,
-            "A PatchCollection was not returned.")
+    def testMultipatch_patchTwoTypes_bothPatched(self):
+        """If we combine two patches, they should be patched together."""
+        # record the old class types
+        Class1Original = Class1
+        Class2Original = Class2
 
+        with multipatch(
+            class1=mock.patch('multipatch_tests.Class1'),
+            class2=mock.patch('multipatch_tests.Class2'),
+        ) as patcher:
+            self.assertIsInstance(Class1(), mock.Mock,
+                "The first class was not patched.")
+            self.assertIsInstance(Class2(), mock.Mock,
+                "The second class was not patched.")
 
-    def testPatch_withTarget_usingContextManager_returnsMock(self):
-        """
-        A context patches within its scope.
-        """
-        with patch('multipatch_tests.Placeholder') as patcher:
-            self.assertTrue(
-                patcher.isStarted, 
-                'The patcher was not started.')
+        self.assertIsInstance(Class1(), Class1Original,
+            "The first class was not reverted.")
+        self.assertIsInstance(Class2(), Class2Original,
+            "The second class was not reverted.")
 
-            placeholder = patcher.Placeholder.return_value
-            placeholder.foo.return_value = 234
-            self.assertEqual(
-                234, Placeholder().foo(),
-                'The mock value was not returned.')
+    def testMultipatch_patchOneType_patched(self):
+        """We can wrap a single patch."""
+        Class1Original = Class1
 
-        self.assertFalse(
-            patcher.isStarted,
-            'The patcher was not stopped automatically.')
+        with multipatch(
+            class1=mock.patch('multipatch_tests.Class1')
+        ) as patcher:
+            self.assertIsInstance(Class1(), mock.Mock,
+                "The class was not patched.")
 
-    def testPatch_withTarget_returnsOriginalOutOfContext(self):
-        """
-        A patcher does nothing until it's started.
-        """
-        patcher = patch('multipatch_tests.Placeholder')
-        self.assertFalse(
-            patcher.isStarted,
-            'The patcher started prematurely.')
+        self.assertIsInstance(Class1(), Class1Original,
+            "The class was not reverted.")
 
-        placeholder = Placeholder()
-        result = placeholder.foo()
-        self.assertEqual(
-            123, result,
-            'The original value was not returned from foo.')
-
-    def testPatch_withTarget_returnsMockInContext(self):
-        """
-        Once started, all patches are applied.
-        """
-        patcher = patch('multipatch_tests.Placeholder')
-        self.assertFalse(
-            patcher.isStarted, 
-            'The patcher was started prematurely.')
+    def testMultipatch_patchType_started_mocksAvailable(self):
+        """Patching a type adds it as an attribute."""
+        patch1 = mock.patch('multipatch_tests.Class1')
+        patcher = multipatch(class1=patch1)
 
         try:
             patcher.start()
-            self.assertTrue(
-                patcher.isStarted,
-                "The patcher was not started.")
-
-            patcher.Placeholder().foo.return_value = 234
-            result = Placeholder().foo()
-            self.assertEqual(234, result, 'The placeholder was not mocked.')
+            self.assertTrue(hasattr(patcher, 'class1'),
+                "The patch was not added as an attribute.")
+    
+            self.assertIsInstance(patcher.class1, mock.Mock,
+                "The patch attribute is not a mock.")
+    
+            self.assertIs(patcher.class1, patcher.class1,
+                "The same mock was not returned for the attribute.")
         finally:
             patcher.stop()
-            self.assertFalse(
-                patcher.isStarted, 
-                'The patcher was not stopped.')
-        
 
-class Placeholder:
-    
-    def foo(self):
-        return 123
+        self.assertFalse(hasattr(patcher, 'class1'),
+            "The patch should not longer be an attribute.")
+
+    def testMultipatch_patchType_notStarted_mocksHidden(self):
+        """We can only see mocks once the context is started."""
+        patch1 = mock.patch('multipatch_tests.Class1')
+        patcher = multipatch(class1=patch1)
+
+        self.assertFalse(hasattr(patcher, 'class1'),
+            "The patcher should not expose the mock until started.")
+
+    def testMultipatch_requestSameAttribute_recieveSameMock(self):
+        """The same mock should be returned for a patch."""
+        with multipatch(
+            class1=mock.patch('multipatch_tests.Class1')
+        ) as patcher:
+            self.assertTrue(hasattr(patcher, 'class1'),
+                "An attribute for the patch was not added.")
+            mock1 = patcher.class1
+            mock2 = patcher.class1
+            self.assertIs(mock1, mock2, 'The same mock was not returned.')
+
+    def testMultipatch_otherMulti_collapses(self):
+        """Support composing by stealing other patches."""
+        patch1 = multipatch(class1=mock.patch('multipatch_tests.Class1'))
+        patch2 = multipatch(class2=mock.patch('multipatch_tests.Class2'))
+        patch = multipatch(patch1, patch2)
+        with patch as patcher:
+            self.assertTrue(hasattr(patcher, 'class1'), 
+                'The first patch was not copied.')
+            self.assertTrue(hasattr(patcher, 'class2'), 
+                'The second patch was not copied.')
+
+    def testMultipatch_duplicatePatch_pm_raisesValueError(self):
+        """Can't share patch names between patch and multipatch."""
+        patch1 = multipatch(class1=mock.patch('multipatch_tests.Class1'))
+        self.assertRaises(
+            ValueError,
+            multipatch,
+            patch1, # has class1
+            class1=mock.patch('multipatch_tests.Class2')
+        )
+
+    def testMultipatch_duplicatePatch_mm_raisesValueError(self):
+        """Can't share patch named between two multipatches."""
+        patch1 = multipatch(class1=mock.patch('multipatch_tests.Class1'))
+        patch2 = multipatch(class1=mock.patch('multipatch_tests.Class1'))
+        self.assertRaises(ValueError, multipatch, patch1, patch2)
+
+    def testMultipatch_treatMultiLikePatch_supportsInterface(self):
+        """Multipatches can be treated like patches."""
+        other = multipatch(class1=mock.patch('multipatch_tests.Class1'))
+        patch = multipatch(other=other)
+        with patch as patcher:
+            self.assertTrue(hasattr(patcher, 'other'),
+                'The multipatcher was not stored as an attribute.')
+            self.assertTrue(hasattr(patcher.other, 'class1'),
+                'The patcher was mocked by accident.')
+            self.assertIsInstance(patcher.other.class1, mock.Mock,
+                'The multipatcher stopped creating mocks.')
+
+
+class Class1(object):
+    pass
+
+
+class Class2(object):
+    pass
